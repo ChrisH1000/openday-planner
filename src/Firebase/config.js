dfbf0be3eec4ba55598783e42fca43dd649bfd31
Chris <ChrisH1000@icloud.com>
2023-05-30 09:07:21 +0800

    updated config to v9 of firebase implementation

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,4 +1,19 @@
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app.options);
+
+/* import firebase from 'firebase/app';
 // import 'firebase/firestore';
 
 firebase.initializeApp({
@@ -11,4 +26,4 @@ firebase.initializeApp({
 });
 console.log(firebase.app().options);
 
-// export const firestore = firebase.firestore();
+// export const firestore = firebase.firestore(); */
