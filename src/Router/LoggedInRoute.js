8657742d91a9ac2ff23f4cc30ea3c1f642cf880e
Chris <ChrisH1000@icloud.com>
2023-06-02 09:44:19 +0800

    added route protection for admin checks for isAdmin

---
+++ src/Router/LoggedInRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
+
+const LoggedInRoute = () => {
+  const auth = getAuth();
+  console.log(auth);
+
+  const loggedIn = true; //auth.currentUser; // determine if authorized, from context or however you're doing it
+  console.log(loggedIn);
+
+  // If authorized, return an outlet that will render child elements
+  // If not, return element that will navigate to login page
+  return loggedIn ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default LoggedInRoute;
