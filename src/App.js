88c3fb6684668b9ca190abdb06dbc7ae80df9d98
Chris <ChrisH1000@icloud.com>
2023-06-16 09:33:02 +0800

    wrapped tree in flowbright component

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import Plans from './Components/Plans';
 import Admin from './Components/Admin';
 import Signup from './Components/Signup';
 import { UserProvider } from './Firebase/UserProvider';
-import './App.css';
+import { Flowbite } from 'flowbite-react';
 
 function App() {
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<LoggedInRoute props="empty" />}>
-            <Route exact path="/" element={<Signup />} />
-          </Route>
-          <Route exact path="/login" element={<LoggedInRoute props="login" />}>
-            <Route exact path="/login" element={<Login />} />
-          </Route>
-          <Route exact path="/signup" element={<LoggedInRoute props="signup" />}>
-            <Route exact path="/signup" element={<Signup />} />
-          </Route>
-          <Route exact path="/plans" element={<LoggedInRoute props="plans" />}>
-            <Route exact path="/plans" element={<Plans />} />
-          </Route>
-          <Route exact path="/admin" element={<LoggedInRoute props="admin" />}>
-            <Route exact path="/admin" element={<Admin />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
+    <Flowbite>
+      <div className="bg-white">
+        <UserProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<LoggedInRoute props="empty" />}>
+                <Route exact path="/" element={<Signup />} />
+              </Route>
+              <Route exact path="/login" element={<LoggedInRoute props="login" />}>
+                <Route exact path="/login" element={<Login />} />
+              </Route>
+              <Route exact path="/signup" element={<LoggedInRoute props="signup" />}>
+                <Route exact path="/signup" element={<Signup />} />
+              </Route>
+              <Route exact path="/plans" element={<LoggedInRoute props="plans" />}>
+                <Route exact path="/plans" element={<Plans />} />
+              </Route>
+              <Route exact path="/admin" element={<LoggedInRoute props="admin" />}>
+                <Route exact path="/admin" element={<Admin />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </BrowserRouter>
+        </UserProvider>
+      </div>
+    </Flowbite>
   );
 }
 
