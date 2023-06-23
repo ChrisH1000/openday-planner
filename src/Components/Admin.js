d835a3b7ef7fd21cd97b162adc70c25784599db2
Chris <ChrisH1000@icloud.com>
2023-06-23 11:41:14 +0800

    get openday and event info from firestore

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -5,15 +5,41 @@ import Header from './Header';
 
 function Admin() {
   useEffect(() => {
+    let opendays = [];
     async function fetchData() {
-      console.log('Admin page');
       const querySnapshot = await getDocs(collection(db, 'openday'));
+      console.log(querySnapshot);
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, ' => ', doc.data());
+        //console.log(doc.id, ' => ', doc.data());
+        opendays.push({ id: doc.id, ...doc.data() });
+
+        /* const querySnapshot2 = await getDocs(collection(db, 'openday', doc.id, 'events'));
+        querySnapshot2.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+        }); */
       });
     }
-    fetchData();
+
+    async function fetchData2(id) {
+      let events = [];
+      const querySnapshot2 = await getDocs(collection(db, 'openday', id, 'events'));
+      querySnapshot2.forEach((doc) => {
+        events.push({ id: doc.id, ...doc.data() });
+      });
+
+      return events;
+    }
+    fetchData().then(() => {
+      console.log(opendays);
+      opendays.forEach((openday) => {
+        fetchData2(openday.id).then((events) => {
+          openday.events = events;
+        });
+      });
+      console.log(opendays);
+    });
   }, []);
 
   return (
