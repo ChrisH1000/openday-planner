7cb0e8bc4db00603306cf85812b60b05b874c26a
Chris <ChrisH1000@icloud.com>
2023-08-16 11:07:25 +0800

    removed flowbright from project

--- src/App.js
+++ src/App.js
@@ -7,41 +7,38 @@ import Admin from './Components/Admin';
 import Signup from './Components/Signup';
 import Header from './Components/Header';
 import { UserProvider } from './Firebase/UserProvider';
-import { Flowbite } from 'flowbite-react';
 
 function App() {
   return (
-    <Flowbite>
-      <UserProvider>
-        <div className="min-h-full">
-          <Header />
-          <main>
-            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-              <BrowserRouter>
-                <Routes>
-                  <Route exact path="/" element={<LoggedInRoute props="empty" />}>
-                    <Route exact path="/" element={<Signup />} />
-                  </Route>
-                  <Route exact path="/login" element={<LoggedInRoute props="login" />}>
-                    <Route exact path="/login" element={<Login />} />
-                  </Route>
-                  <Route exact path="/signup" element={<LoggedInRoute props="signup" />}>
-                    <Route exact path="/signup" element={<Signup />} />
-                  </Route>
-                  <Route exact path="/plans" element={<LoggedInRoute props="plans" />}>
-                    <Route exact path="/plans" element={<Plans />} />
-                  </Route>
-                  <Route exact path="/admin" element={<LoggedInRoute props="admin" />}>
-                    <Route exact path="/admin" element={<Admin />} />
-                  </Route>
-                  <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-              </BrowserRouter>
-            </div>
-          </main>
-        </div>
-      </UserProvider>
-    </Flowbite>
+    <UserProvider>
+      <div className="min-h-full">
+        <Header />
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <BrowserRouter>
+              <Routes>
+                <Route exact path="/" element={<LoggedInRoute props="empty" />}>
+                  <Route exact path="/" element={<Signup />} />
+                </Route>
+                <Route exact path="/login" element={<LoggedInRoute props="login" />}>
+                  <Route exact path="/login" element={<Login />} />
+                </Route>
+                <Route exact path="/signup" element={<LoggedInRoute props="signup" />}>
+                  <Route exact path="/signup" element={<Signup />} />
+                </Route>
+                <Route exact path="/plans" element={<LoggedInRoute props="plans" />}>
+                  <Route exact path="/plans" element={<Plans />} />
+                </Route>
+                <Route exact path="/admin" element={<LoggedInRoute props="admin" />}>
+                  <Route exact path="/admin" element={<Admin />} />
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </main>
+      </div>
+    </UserProvider>
   );
 }
 
