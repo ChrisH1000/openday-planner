203b6146ce3678d386974f9d19e585af602990f2
Chris <ChrisH1000@icloud.com>
2023-08-25 10:06:28 +0800

    new addopenday script and route

--- src/App.js
+++ src/App.js
@@ -18,22 +18,23 @@ function App() {
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <BrowserRouter>
               <Routes>
-                <Route exact path="/" element={<LoggedInRoute props="empty" />}>
-                  <Route exact path="/" element={<Signup />} />
+                <Route path="/" element={<LoggedInRoute props="empty" />}>
+                  <Route path="/" element={<Signup />} />
                 </Route>
-                <Route exact path="/login" element={<LoggedInRoute props="login" />}>
-                  <Route exact path="/login" element={<Login />} />
+                <Route path="/login" element={<LoggedInRoute props="login" />}>
+                  <Route path="/login" element={<Login />} />
                 </Route>
-                <Route exact path="/signup" element={<LoggedInRoute props="signup" />}>
-                  <Route exact path="/signup" element={<Signup />} />
+                <Route path="/signup" element={<LoggedInRoute props="signup" />}>
+                  <Route path="/signup" element={<Signup />} />
                 </Route>
-                <Route exact path="/plans" element={<LoggedInRoute props="plans" />}>
-                  <Route exact path="/plans" element={<Plans />} />
+                <Route path="/plans" element={<LoggedInRoute props="plans" />}>
+                  <Route path="/plans" element={<Plans />} />
                 </Route>
-                <Route exact path="/admin" element={<LoggedInRoute props="admin" />}>
-                  <Route exact path="/admin" element={<Admin />}>
-                    <Route exact path="addopenday" element={<AddOpenday />} />
-                  </Route>
+                <Route path="/admin" element={<LoggedInRoute props="admin" />}>
+                  <Route path="/admin" element={<Admin />} />
+                </Route>
+                <Route path="/admin/addopenday" element={<LoggedInRoute props="addopenday" />}>
+                  <Route path="/admin/addopenday" element={<AddOpenday />} />
                 </Route>
                 <Route path="*" element={<Navigate to="/" />} />
               </Routes>
--- src/Components/AddOpenday.js
+++ src/Components/AddOpenday.js
@@ -1,128 +1,108 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-// import { Navigate, Link } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function AddOpenday() {
   const { register, handleSubmit } = useForm();
-  const [isLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
   const onSubmit = async (data) => {
     console.log(isLoading);
     console.log(data);
-    /* let newUser;
-    setLoading(true);
-    try {
-      newUser = await signup(data);
-      reset();
-
-      if (newUser) {
-        <Navigate to="/plans" />;
-      } else {
-        setLoading(false);
-      }
-    } catch (error) {
-      console.log(error);
-    } */
+    setLoading(false);
   };
 
   return (
-    <div className="login-container">
-      <div className="ui card login-card">
-        <div className="content">
-          <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <div className="mb-2 block">
-                <label htmlFor="fname" className="inputLabel">
-                  Title
-                </label>
+    <>
+      <h1 className="text-2xl mb-8">Add Openday</h1>
+      <div className="addopenday-container">
+        <div className="ui card login-card">
+          <div className="content">
+            <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="title" className="inputLabel">
+                    Title
+                  </label>
+                </div>
+                <div className="inputWrapper">
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    required
+                    className="textInput"
+                    {...register('title')}
+                  />
+                </div>
               </div>
-              <div className="inputWrapper">
-                <input
-                  type="text"
-                  name="fname"
-                  id="fname"
-                  required
-                  className="textInput"
-                  {...register('firstName')}
-                />
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="location" className="inputLabel">
+                    Location
+                  </label>
+                </div>
+                <div className="inputWrapper">
+                  <input
+                    type="text"
+                    name="location"
+                    id="location"
+                    required
+                    className="textInput"
+                    {...register('location')}
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <label htmlFor="lname" className="inputLabel">
-                  Last Name
-                </label>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="email1" className="inputLabel">
+                    Your email
+                  </label>
+                </div>
+                <div className="inputWrapper">
+                  <input
+                    type="email"
+                    name="email1"
+                    id="email1"
+                    required
+                    className="textInput"
+                    placeholder="john@doe.com"
+                    {...register('email')}
+                  />
+                </div>
               </div>
-              <div className="inputWrapper">
-                <input
-                  type="text"
-                  name="lname"
-                  id="lname"
-                  required
-                  className="textInput"
-                  {...register('lastName')}
-                />
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="password1" className="inputLabel">
+                    Your password
+                  </label>
+                </div>
+                <div className="inputWrapper">
+                  <input
+                    type="password"
+                    name="password1"
+                    id="password1"
+                    required
+                    className="textInput"
+                    {...register('password')}
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <label htmlFor="email1" className="inputLabel">
-                  Your email
-                </label>
+              <div className="mt-6 flex items-center justify-end gap-x-6">
+                <Link to="/admin" className="btn bg-red-600 hover:bg-red-700 focus:ring-red-500">
+                  Cancel
+                </Link>
+                <button
+                  type="submit"
+                  className="btn bg-green-600 hover:bg-green-700 focus:ring-green-500">
+                  Save
+                </button>
               </div>
-              <div className="inputWrapper">
-                <input
-                  type="email"
-                  name="email1"
-                  id="email1"
-                  required
-                  className="textInput"
-                  placeholder="john@doe.com"
-                  {...register('email')}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <label htmlFor="password1" className="inputLabel">
-                  Your password
-                </label>
-              </div>
-              <div className="inputWrapper">
-                <input
-                  type="password"
-                  name="password1"
-                  id="password1"
-                  required
-                  className="textInput"
-                  {...register('password')}
-                />
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <input
-                id="remember"
-                name="remember"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-              />
-              <label htmlFor="remember" className="inputLabel">
-                Remember me
-              </label>
-            </div>
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-              <Link to="/login" className="btn bg-gray-600">
-                Login
-              </Link>
-              <button type="submit" className="btn bg-indigo-600">
-                Signup
-              </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
