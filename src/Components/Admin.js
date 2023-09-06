c7619b9dd9cb5a4905e60189f83a44d28e205479
Chris <ChrisH1000@icloud.com>
2023-09-07 07:47:29 +0800

    added link for edit route

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -76,13 +76,11 @@ function Admin() {
               return (
                 <tr
                   key={openday.id}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                >
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                   <th
                     scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    {openday.title}
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <Link to="/admin/editopenday/{openday.id}">{openday.title}</Link>
                   </th>
                   <td className="px-6 py-4">{startDate}</td>
                   <td className="px-6 py-4">{openday.location}</td>
